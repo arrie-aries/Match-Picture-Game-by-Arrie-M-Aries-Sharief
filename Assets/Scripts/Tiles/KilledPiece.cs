175e5178d78cd6442c0ae910ec1199a270730979
Arrie Rain <arrie.sharief@gmail.com>
2022-09-05 17:11:20 +0800

    Bug fix, namespace, and codestlye

--- Assets/Scripts/Tiles/KilledPiece.cs
+++ Assets/Scripts/Tiles/KilledPiece.cs
@@ -3,38 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class KilledPiece : MonoBehaviour
+namespace MatchPicture.Tile.Piece
 {
-    public bool falling;
-
-    float speed = 16f;
-    float gravity = 32f;
-    Vector2 moveDir;
-    RectTransform rect;
-    Image img;
-
-    public void Initialize(Sprite piece, Vector2 start)
+    public class KilledPiece : MonoBehaviour
     {
-        falling = true;
+        public bool falling;
 
-        moveDir = Vector2.up;
-        moveDir.x = Random.Range(-1.0f, 1.0f);
-        moveDir *= speed / 2;
+        float speed = 16f;
+        float gravity = 32f;
+        Vector2 moveDir;
+        RectTransform rect;
+        Image img;
 
-        img = GetComponent<Image>();
-        rect = GetComponent<RectTransform>();
-        img.sprite = piece;
-        rect.anchoredPosition = start;
-    }
+        public void Initialize(Sprite piece, Vector2 start)
+        {
+            falling = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!falling) return;
-        moveDir.y -= Time.deltaTime * gravity;
-        moveDir.x = Mathf.Lerp(moveDir.x, 0, Time.deltaTime);
-        rect.anchoredPosition += moveDir * Time.deltaTime * speed;
-        if (rect.position.x < -64f || rect.position.x > Screen.width + 64f || rect.position.y < -64f || rect.position.y > Screen.height + 64f)
-            falling = false;
+            moveDir = Vector2.up;
+            moveDir.x = Random.Range(-1.0f, 1.0f);
+            moveDir *= speed / 2;
+
+            img = GetComponent<Image>();
+            rect = GetComponent<RectTransform>();
+            img.sprite = piece;
+            rect.anchoredPosition = start;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!falling) return;
+            moveDir.y -= Time.deltaTime * gravity;
+            moveDir.x = Mathf.Lerp(moveDir.x, 0, Time.deltaTime);
+            rect.anchoredPosition += moveDir * Time.deltaTime * speed;
+            if (rect.position.x < -64f || rect.position.x > Screen.width + 64f || rect.position.y < -64f || rect.position.y > Screen.height + 64f)
+                falling = false;
+        }
     }
 }
